75a04fe69eb8d42011a3e0c9bae9312983d84a41
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-11 22:33:14 +1100

    Method 2 using useContext hook

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,31 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./collection.styles.scss";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import CollectionsContext from "../../contexts/collections/collections.context";
 
+// useContext is a hook that allows to get vlue fromn the context, and store as a const inside teh component
 const CollectionPage = ({ match }) => {
-  return (
-    // Method 1 - Context
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
+  // Method 2 - Context using useContext
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
 
-        // return the HTML element
-        return (
-          <div className="collection-page">
-            <h2 className="title">{title}</h2>
-            <div className="items">
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
+  // return the HTML element
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
